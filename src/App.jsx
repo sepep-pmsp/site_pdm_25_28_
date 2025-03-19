326822fef12de170e217deba0f14f3762a82015f
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-03-19 16:52:57 -0300

    Banner (#2)
    * create banner
    
    * update
    
    * moved and creat new img

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import Banner from './components/banner/banner';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <div className='home-app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='content'>
+        <div className='banner'>
+          <Banner />
+        </div>
+      </div>
     </div>
   );
 }
