880abb05035e3145c684e47458fa2c565483b6c5
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-03-27 18:16:23 -0300

    modificado textos dos eixos

--- src/pages/Home/SectionEixos.jsx
+++ src/pages/Home/SectionEixos.jsx
@@ -21,7 +21,9 @@ const SectionEixos = () => {
   useEffect(() => {
     const eixoId = searchParams.get("eixo");
     if (eixoId) {
-      const eixoEncontrado = eixosData.find((eixo) => eixo.url.toString() === eixoId);
+      const eixoEncontrado = eixosData.find(
+        (eixo) => eixo.url.toString() === eixoId
+      );
       if (eixoEncontrado) {
         setSelectedEixo(eixoEncontrado);
       }
@@ -31,11 +33,16 @@ const SectionEixos = () => {
   return (
     <section className="py-12 flex items-center justify-center flex-nowrap">
       <div className="mx-auto px-4 flex flex-col items-center justify-center flex-nowrap w-full gap-12">
-        <h2 className="text-3xl font-bold text-center mb-8">Eixos do PDM 2025-2028</h2>
+        <h2 className="text-4xl font-bold text-center mb-8">
+          Eixos do PDM 2025-2028
+        </h2>
         <div className="w-[50rem] div-eixo-texto ">
-            <p className="text-3xl font-light text-justify eixo-texto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ligula elit, ultricies nec lacinia quis, laoreet ac ex. Fusce malesuada molestie leo a varius. In et sem feugiat, ultricies dolor nec, fermentum nisi. Aliquam erat volutpat. Curabitur vel hendrerit ex, at pretium arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus sit amet mauris tincidunt, imperdiet nisl eget, vehicula tellus. Proin tincidunt, nunc sed sollicitudin blandit, ex nisl tincidunt ligula, in scelerisque quam turpis nec elit. Maecenas vulputate blandit ligula ut vulputate. Maecenas luctus erat est, sodales sagittis enim volutpat porta. Cras tincidunt pellentesque ante ac auctor. Nullam sed eleifend felis. Morbi at eros nec turpis ullamcorper 
-            </p>
+          <p className="text-3xl font-light text-justify eixo-texto">
+            São quatro eixos organizados de ações estratégicas para o próximo
+            quadriênio. São pilares essenciais para a construção de uma cidade
+            inclusiva. Os quatro eixos são complementares às demais diretrizes
+            da gestão municipal.
+          </p>
         </div>
 
         <div className="card-grid sm:grid-cols-2 md:grid-cols-3 lg:flex flex-wrap flex-row justify-center items-center gap-30">
