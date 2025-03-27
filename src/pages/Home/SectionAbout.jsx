b9c097044d1897588a23c19a767cbcbd3141c77e
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-03-27 18:16:34 -0300

    arrumando css e trocado os textos

--- src/pages/Home/SectionAbout.jsx
+++ src/pages/Home/SectionAbout.jsx
@@ -1,32 +1,42 @@
 import CustomButton from "../../components/button/button";
-import logo from "../../assets/images-png/pdm-completo-branco.png";
+import logo from "../../assets/images/pdm-completo-branco.png";
 
 export default function About() {
   return (
     <div className="flex items-center justify-evenly flex-nowrap flex-row">
       <div className="w-[38rem] flex flex-col justify-center items-center gap-8">
-        <p className="text-xl">
-          O Programa de Metas 2025-2028 representa um compromisso estratégico da
-          gestão municipal com a população. Ele estabelece objetivos claros,
-          alinhados às demandas sociais, para promover desenvolvimento
-          sustentável, eficiência administrativa e qualidade de vida para os
-          cidadãos.
+        <p className="text-3xl text-justify font-extralight">
+          O que esperar do governo que inicia um mandato? O que de fato será
+          feito, quando e por quais meios? São perguntas legítimas dos cidadãos
+          e que pedem um posicionamento claro do poder público. Esse
+          posicionamento, em São Paulo, é apresentado à sociedade – e com a
+          participação dela – na forma de um Programa de Metas quadrienal que
+          sela definitivamente o compromisso do prefeito e de sua equipe com a
+          população.
         </p>
-        <p className="text-xl">
-          Composto por eixos temáticos definidos a partir da participação
-          popular, o plano orienta investimentos prioritários e ações
-          estruturantes em áreas fundamentais, garantindo maior transparência e
-          previsibilidade nas políticas públicas.
+        <p className="text-3xl text-justify font-extralight">
+          Além de dar transparência às políticas prioritárias da Prefeitura, o
+          PdM permite às diversas secretarias uma visão sistêmica da gestão que
+          evita a sobreposição de medidas, otimiza recursos e facilita execuções
+          em conjunto. Em seu escopo estão realizações tangíveis, mensuráveis,
+          cabíveis em uma perspectiva de quatro anos.
         </p>
-        <p className="text-xl">
-          A metodologia adotada busca integração entre setores, otimização de
-          recursos e acompanhamento contínuo dos indicadores. Dessa forma, o
-          Programa de Metas se torna um instrumento essencial para monitoramento
-          e avaliação dos compromissos assumidos pela administração municipal.
+        <p className="text-3xl text-justify font-extralight">
+          Internamente, é também uma ferramenta de monitoramento de ações e de
+          análise de desempenho. Na medida em que traz a público essas
+          avaliações, dá externamente, aos cidadãos, a possibilidade de
+          fiscalizar a gestão e cobrar resultados, que devem ser, sempre que
+          possível, não apenas atingidos, mas superados. O compromisso público
+          que é a marca do Programa de Metas não é só com a execução: é com a
+          execução eficiente e de qualidade.
         </p>
       </div>
       <div className="w-auto h-[27rem] flex flex-col flex-nowrap justify-center items-center gap-20 px-4 py-8 rounded-2xl btn-dwnld">
-        <CustomButton type="download" label="clique aqui para fazer  o download da versão inicial do PDM 2025-2028 " target="/manual.pdf" />
+        <CustomButton
+          type="download"
+          label="clique aqui para fazer  o download da versão inicial do PDM 2025-2028 "
+          target="/manual.pdf"
+        />
         <img src={logo} alt="" className="w-90 img-about" />
       </div>
     </div>
