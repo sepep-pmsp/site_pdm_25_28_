8b544ed3d7a347169c2c64ce61f41933ffc1feed
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-03-27 18:14:07 -0300

    created timeline para intro dos eixos

--- src/components/Intro/SectionIntro.jsx
+++ src/components/Intro/SectionIntro.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import modalData from "../../data/modalContent.json";
-import ModalAudiencias from "../modal/ModalAudiences";
-import img_particeMais from "../../assets/images-svg/participe_mais.svg";
+import img_particeMais from "../../assets/svg/participe_mais.svg";
+import BannerSecom from "../banner/banner_secom";
+import Timeline from "./timeline";
 
 const Introducao = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Verifica se os dados da introdução estão carregados corretamente
   if (!modalData.introducao) {
@@ -14,7 +13,7 @@ const Introducao = () => {
   return (
     <div className="introducao-container flex flex-col flex-nowrap justify-center items-center gap-20">
       <div>
-        <h1 className="text-6xl">{modalData.introducao.titulo}</h1>
+        <h1 className="text-5xl">{modalData.introducao.titulo}</h1>
       </div>
 
       <div className="w-[45rem] text-justify text-[1.8rem] flex flex-col items-center justify-center gap-12">
@@ -25,26 +24,28 @@ const Introducao = () => {
         ))}
       </div>
 
-      <div>
-        <button className="btn-audiences" onClick={() => setIsModalOpen(true)}>
-          Saiba mais sobre o processo participativo no PDM
-        </button>
-        <ModalAudiencias
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
+      <div className="timeline flex flex-col flex-nowrap items-center justify-center gap-24">
+        <div>
+            <h2 className="text-4xl">Como são elaborados:</h2>
+        </div>
+        <div>
+            <Timeline />
+        </div>
       </div>
       <div className="w-full h-[17rem] flex items-center justify-center flex-col flex-nowrap">
-        <a href="https://exemplo.com" className="btn-audience-intro">
+        <a href="https://participemais.prefeitura.sp.gov.br/" className="btn-audience-intro" target="_blank">
           <div className="image-container">
             <div>
                 <img src={img_particeMais} alt="Descrição" />
             </div>
             <div>
-                <p className="button-text text-center">ver processos participativos abertos no participe+</p>
+                <p className="button-text text-center text-3xl font-bold uppercase">contribua para o pdm</p>
             </div>
           </div>
         </a>
+      </div>
+      <div>
+        <BannerSecom />
       </div>
     </div>
   );
